c9563b908e9431212466724e9aae8fe51a70feaa
Assper <TheAssper@gmail.com>
2023-07-05 01:02:11 +0300

    remove hardcoded indent

--- src/lib/components/Layout.svelte
+++ src/lib/components/Layout.svelte
@@ -8,8 +8,8 @@
     width: 100%;
     max-width: 1200px;
     min-width: 480px;
-    padding: 15px;
-    border-radius: 15px;
+    padding: var(--indent-small);
+    border-radius: var(--indent-small);
     background-color: white;
     box-shadow: 0px 0px 10px 1px rgba(200,200,200,0.4);
   }
